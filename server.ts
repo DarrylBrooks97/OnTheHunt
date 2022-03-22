1f4ba4085e7ce0eb59e0c36a666f68b0ba7a9009
Darryl Brooks <darrylbrooks13@gmail.com>
2022-03-22 17:54:44 -0500

    revert: replacing change

--- server.ts
+++ server.ts
@@ -1,8 +1,4 @@
-import path from 'path';
+import * as build from '@remix-run/dev/server-build';
 import { createRequestHandler } from '@remix-run/vercel';
-const BUILD_DIR = path.join(process.cwd(), 'public/build');
 
-export default createRequestHandler({
-	build: require(BUILD_DIR),
-	mode: process.env.NODE_ENV,
-});
+export default createRequestHandler({ build, mode: process.env.NODE_ENV });
