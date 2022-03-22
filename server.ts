341a5dfa5186cf7259037cab7e7b714584548090
Darryl Brooks <darrylbrooks13@gmail.com>
2022-03-22 17:50:34 -0500

    refactor: updating with local build

--- server.ts
+++ server.ts
@@ -1,4 +1,8 @@
+import path from 'path';
 import { createRequestHandler } from '@remix-run/vercel';
-import * as build from '@remix-run/dev/server-build';
+const BUILD_DIR = path.join(process.cwd(), 'public/build');
 
-export default createRequestHandler({ build, mode: process.env.NODE_ENV });
+export default createRequestHandler({
+	build: require(BUILD_DIR),
+	mode: process.env.NODE_ENV,
+});
